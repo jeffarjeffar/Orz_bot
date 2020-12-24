cb39ae6834fd107ea1c675d1b6e49fc9f65fb8e9
jeffarjeffar <jeffarjeffar@gmail.com>
2020-12-24 08:05:43 -0800

    Fixed the bugs, made embeds

--- Orz_bot/Orz_bot.py
+++ Orz_bot/Orz_bot.py
@@ -21,7 +21,7 @@ def is_admin(person):
 async def on_ready():
     await send_to_gallery()
     print('Bot is ready.')
-    await client.change_presence(activity=discord.Game(name='TMW ORZ'))
+    await client.change_presence(activity=discord.Game(name='Tmw orz'))
 
 
 messages = []
@@ -30,8 +30,8 @@ messages = []
 async def on_message(message):
     messages.append(message)
 
-    gallery = client.get_emoji(791402385436180500)
-    await message.add_reaction(gallery)
+    #gallery = client.get_emoji(791402385436180500)
+    #await message.add_reaction(gallery)
 
     if message.author == client.user:
         return
@@ -57,30 +57,40 @@ async def on_message(message):
             await message.channel.send('Pong!')
 
 async def send_to_gallery():
-    print('Watching for stars')
+    #print('Watching for stars')
 
     gallery = client.get_emoji(791402385436180500)
 
     channel = client.get_channel(791367434166468638) # channel ID goes here
     while True:
-        print('Processing', len(messages), 'messages')
+        #print('Processing', len(messages), 'messages')
         for msg in messages:
             cnt = 0
             updated_message = await msg.channel.fetch_message(msg.id)
-            print('Found message', updated_message)
+            #print('Found message', updated_message)
             for reaction in updated_message.reactions:
-                print('Found reaction', reaction, ', count = ', reaction.count)
-                print(str(reaction.emoji))
+                #print('Found reaction', reaction, ', count = ',
+                #reaction.count)
+                #print(str(reaction.emoji))
                 if str(reaction.emoji) == '<:gallery:791402385436180500>':
-                    print('Found stars')
+                    #print('Found stars')
                     cnt = reaction.count
 
-            print(cnt, 'people starred')
-            if cnt > 1:
-                await channel.send(msg.content)
-        print('Messages sent')
+            #print(cnt, 'people starred')
+            if cnt > 0:
+                #print(msg.author, msg.content)
+                embedVar = discord.Embed(
+                   title = str(msg.author), 
+                   description = str(msg.content),
+                   color = discord.Color.blue(),
+                   url = msg.jump_url,
+                   author = msg.author)
+                
+                await channel.send(embed=embedVar)
+                await updated_message.clear_reaction(gallery)
+        #print('Messages sent')
         messages.clear()
-        await asyncio.sleep(60) # task runs every 60 seconds
+        await asyncio.sleep(30) # task runs every x seconds
 
 client.run('insert super secret orz token here')
 
